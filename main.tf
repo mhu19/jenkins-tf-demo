26c386645b6b45ca386f4ce17684ca187d2b0c8f
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-05-19 22:15:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,18 @@ provider "google" {
   project = var.project
   region  = var.region
   zone    = var.zone
-
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-vpcnetwork"
+}
+
+resource "google_sql_database_instance" "database" {
+  name = "db-test"
+  database_version = "POSTGRES_11"
+  region = "us-central1"
+  
+  settings {
+    tier = "db-f1-micro"
+  }
 }
