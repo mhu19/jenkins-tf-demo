0e45856aa90a513dcde876148aa87b9452403be6
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-05-20 00:19:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,33 @@ provider "google" {
   zone    = var.zone
 }
 
+module "bastion_host" {
+  source = "terraform-google-modules/bastion-host/google//modules/bastion-group"
+  
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+  
+  network = google_compute_network.network.self_link
+  subnet = google_compute_subnetwork.subnet.self_link
+  members = ["jenkins-sa@crack-mix-350403.iam.gserviceaccount.com"]
+}
+  
 resource "google_compute_network" "vpc_network" {
   name = "terraform-vpcnetwork"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name = "terraform-subnet"
+  ip_cidr_range = "10.127.0.0/20
+  network = google_compute_network.network.self_link
+  private_ip_google_access = true
 }
 
 //database
 resource "google_sql_database_instance" "main" {
-  name = "db-test"
+  name = "db-test1"
   database_version = "POSTGRES_11"
   region = "us-central1"
   
@@ -42,3 +62,5 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   }
 }
 
+  
+  
