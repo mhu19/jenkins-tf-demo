0b2cd9ba66255cd39e8148b03ea74ae041d98866
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-05-19 22:53:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-vpcnetwork"
 }
 
+//database
 resource "google_sql_database_instance" "database" {
   name = "db-test"
   database_version = "POSTGRES_11"
@@ -16,4 +17,28 @@ resource "google_sql_database_instance" "database" {
   settings {
     tier = "db-f1-micro"
   }
+  deletion_protection = false
 }
+
+resource "google_container_cluster" "primary" {
+  name = "gke-cluster"
+  location = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count = 1
+}
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name = "node-pool"
+  location =  "us-central1"
+  cluster = google_container_cluster.primary.name
+  node_count = 1
+  
+  node_config {
+    preemptible = true
+    machine_type = "e2-medium"
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
