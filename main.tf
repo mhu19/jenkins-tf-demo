93e37e36f92a1056a632870ae66b7f5f6a9a9f69
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-05-19 20:58:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 2.5"
-    }
-  }
-}
-
 provider "google" {
   project = var.project
   region  = var.region
